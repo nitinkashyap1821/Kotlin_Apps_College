43c6dc4df5e93a72a009047102e43d806255b821
nitinkashyap1821 <76529658+nitinkashyap1821@users.noreply.github.com>
2021-07-03 18:23:00 +0530

    MAD Lab_7

---
+++ TextToSpeech/app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.texttospeech
+
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mTTS: TextToSpeech
+    private lateinit var mEditText: EditText
+    private lateinit var mButtonSpeak: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mButtonSpeak = findViewById(R.id.button_speak)
+        mEditText = findViewById(R.id.edit_text)
+
+        mTTS = TextToSpeech(this, OnInitListener { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result = mTTS.setLanguage(Locale.US)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this@MainActivity, "Language Not Supported", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "Initialization Failed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        })
+        mButtonSpeak.setOnClickListener {
+            val text = mEditText.text.toString()
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+        }
+    }
+
+    override fun onDestroy() {
+        mTTS.stop()
+        mTTS.shutdown()
+        super.onDestroy()
+    }
+
+
+}
